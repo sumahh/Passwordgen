dd3d56f8374c922bca8107feaf3c61cc3238d5d4
sumahh <tkanyingi23@gmail.com>
2021-04-08 17:48:55 +0300

    styled a lil bit

--- gen.js
+++ gen.js
@@ -1,8 +1,12 @@
-const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-let pswd = '';
+function password () {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let pswd = '';
 
-for(let i = 0; i < 32; i++) {
-    pswd += alphabet.charAt(Math.floor(Math.random()*63));
+    for(let i = 0; i < 32; i++) {
+        pswd += alphabet.charAt(Math.floor(Math.random()*63));
+    }
+
+    //console.log(pswd);
+    document.getElementById("pswrd").innerHTML = pswd;
 }
-
-console.log(pswd);
+password();
